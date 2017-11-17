0ce45c2b4e55fe87287f98bc9e9c099c607da174
Chirag <chiragjn120@gmail.com>
2017-11-17 13:21:01 +0530

    f

--- checking.sh
+++ checking.sh
@@ -6,39 +6,39 @@ a=`expr 11`
 # for loop
 for ((i=0;i<=$a;i++))
 do
-	echo $i
+	#echo $i
+	b=`expr 1`
 done
 
 # for loop using seq
 for j in `seq 1 10`
 do
-	echo $j
+	#echo $j
+	b=`expr 1`
 done
 
 # while loop
 counter=0
 while [ $counter -lt 10 ]
 do
-	echo $counter
+	#echo $counter
 	let counter=counter+1
 done
 
-# until loop
-counter_until=0
-until [ $counter_until -lt 10 ]
+# multiple files in bash
+for i in {1..3}
 do
-	
-	echo $counter_until
-	let counter_until=counter_until+1
+	cat "exam$i.txt" >> student.txt
+	echo >> student.txt
 done
-
-# if loop
-
-
-# switch case
-
 
 # arrays in bash
 # read -a makes array from input
+declare -a myarr
+myarr[0]=1
+myarr[1]=2
+echo ${myarr[0]}
+echo ${myarr[*]}
 
-
+# sortinh
+sort -k 4 -V input.txt > output.txt
